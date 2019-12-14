fd232546ff8a5915600c7661db2a67b13d5c811d
Álvaro <36482489+alvaroolt@users.noreply.github.com>
2019-12-14 18:44:29 +0100

    Corrección parte 4 de probando arrays - tema 3

--- tema3/probandoArrays/parte4/codigo4.js
+++ tema3/probandoArrays/parte4/codigo4.js
@@ -1,29 +1,30 @@
-let info;
-
-document.addEventListener("DOMContentLoaded", iniciar)
-
 function iniciar() {
-    info = document.getElementById("info");
+    let info = document.getElementById("info");
+    document.getElementById("boton").addEventListener("click", clicarBoton);
+}
+
+function clicarBoton() {
+    info.innerHTML = ""; //así logro que cada vez que se clica el botón, se limpie el mensaje para volver a escribir
     mostrarArray();
 }
 
+function generarArray(dimension) {
+    if (dimension < 1) {
+        info.innerHTML = "Dimension no válida.";
+        return;
+    }
+    let array = new Array(dimension);
+    for (let i = 0; i < dimension; i++) {
+        array[i] = i;
+    }
+    return array;
+}
+
 function mostrarArray() {
-    let dimension = 2;
-    let tamano = 11;
-    let array = ejecutarArray(dimension, tamano);
-    info.innerHTML = "Dimensiones: " + dimension + ". Tamaño: " + tamano + "<br/>";
-    array.forEach(function(value, index){
+    let array = generarArray(document.getElementById("num").value);
+    array.forEach(function (value, index) {
         info.innerHTML += "Índice: " + index + ". Valor: " + value + "<br/>";
     });
 }
 
-function ejecutarArray(dimension, tamano) {
-    let array = new Array(dimension);
-    for (let i = 0; i < array.length; i++) {
-        array[i] = new Array(tamano);
-        for (let j = 0; j < array[i].length; j++) {
-            array [i][j] = j;
-        } 
-    }
-    return array;
-}
+document.addEventListener("DOMContentLoaded", iniciar);
